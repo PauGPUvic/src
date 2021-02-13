fc55b702148afdb13b17db2735897299172f711f
PauGPUvic <50375460+PauGPUvic@users.noreply.github.com>
2021-02-13 17:31:32 +0100

    Add HolaFarola

---
+++ javaclass/HolaFarola.java
@@ -0,0 +1,24 @@
+package javaclass;
+
+public class HolaFarola {
+	
+	String msg;
+	
+
+	public HolaFarola() {
+		// TODO Auto-generated constructor stub
+		
+		msg = "hola";
+	}
+
+
+	public String getMsg() {
+		return msg;
+	}
+
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+}
