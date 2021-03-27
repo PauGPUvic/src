6e1441f662f4bd0f1dac8278c64e498a3db53bc4
PauGPUvic <50375460+PauGPUvic@users.noreply.github.com>
2021-03-27 20:17:13 +0100

    Documentation

--- javaclass/Jugador.java
+++ javaclass/Jugador.java
@@ -1,12 +1,30 @@
 package javaclass;
 
+
+
+/**
+ * <h1> Create a player</h1>
+ * The classe jugador implements diferent methos and params that allows the player join a match
+ * @author Pau Grau Palomar
+ * @version 1.0
+ * 
+ * 
+ * */
 public class Jugador extends Persona {
-		
+	
+	
 	private String color;
 	private String avatar;
 	private String nickname;
 	private int puntuacio_total;
 	
+	
+	/**
+	 * @param color  String components that define which color uses
+	 * @param avatar String component that defines the name of avatar used for the player
+	 * @param nickname String component that define the display name of the player
+	 * @param puntuacio_total int component that define the current points of the player
+	 */
 	public  Jugador(String col, String av, String nick, int punt, String nm, int edt, boolean carnet) {
 			
 		super(nm,edt,carnet);
@@ -19,28 +37,52 @@ public class Jugador extends Persona {
 	
 	
 
-
+	/**
+	 * @return String  return the color code
+	 **/
 	public String getColor() {
 		return color;
 	}
+	
+	/**
+	 * @param color  String components that define which color uses
+	 **/
 	public void setColor(String color) {
 		this.color = color;
 	}
+	/**
+	 * @return String  return  the name of the avatar
+	 **/
 	public String getAvatar() {
 		return avatar;
 	}
+	/**
+	 * @param avatar  String components that define which color uses
+	 **/
 	public void setAvatar(String avatar) {
 		this.avatar = avatar;
 	}
+	/**
+	 * @return String  return the display name of the player
+	 **/
 	public String getNickname() {
 		return nickname;
 	}
+	/**
+	 * @param nickname  String components that define which color uses
+	 **/
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+	/**
+	 * @return int  return the points of the current player
+	 **/
 	public int getPuntuacio_total() {
 		return puntuacio_total;
 	}
+	/**
+	 * @param puntuacio_total  int components that define which color uses
+	 **/
 	public void setPuntuacio_total(int puntuacio_total) {
 		this.puntuacio_total = puntuacio_total;
 	}
--- javaclass/Partida.java
+++ javaclass/Partida.java
@@ -1,4 +1,12 @@
 package javaclass;
+/**
+ * <h1> Create a match</h1>
+ * The classe Partida generates a match with two players
+ * @author Pau Grau Palomar
+ * @version 1.0
+ * 
+ * 
+ * */
 
 public class Partida implements Activitat{
 
@@ -7,13 +15,17 @@ public class Partida implements Activitat{
 	private Jugador Player2;
 
 	
-	
+	/**
+	 * @return String  return the name of the match
+	 **/
 	public String getMatch_Name() {
 		return match_Name;
 	}
 
 
-
+	/**
+	 * @param match_Name String  set the name of the match
+	 **/
 	public void setMatch_Name(String match_Name) {
 		this.match_Name = match_Name;
 	}
@@ -21,31 +33,41 @@ public class Partida implements Activitat{
 
 
 	
-
+	/**
+	 * @return Player1  return the Jugador object of player1
+	 **/
 	public Jugador getPlayer1() {
 		return Player1;
 	}
 
 
-
+	/**
+	 * @param player1  set  the first player  of the match
+	 **/
 	public void setPlayer1(Jugador player1) {
 		Player1 = player1;
 	}
 
 
-
+	/**
+	 * @return Player2  return the Jugador object of player2
+	 **/
 	public Jugador getPlayer2() {
 		return Player2;
 	}
 
 
-
+	/**
+	 * @param player2  set  the second player  of the match
+	 **/
 	public void setPlayer2(Jugador player2) {
 		Player2 = player2;
 	}
 
 
-
+	/**
+	 *  Calculates who wins
+	 **/
 	@Override
 	public String QuiGuanya() {
 		// TODO Auto-generated method stub
